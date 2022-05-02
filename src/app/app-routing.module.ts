d0d3ded84b715010962a6eefdb42b5fe7ba4cd6d
olivierbasquin <olivier.basquin.iphone@gmail.com>
2022-05-02 12:10:55 +0200

    first commit 1ere maquette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+/* définir les routes de l'applications */
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'details', component: DetailComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ModelsComponent } from './shared/models/models.component';
+import { ServicesComponent } from './shared/services/services.component';
+import { FilterbarComponent } from './filterbar/filterbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    ListComponent,
+    SearchbarComponent,
+    ModelsComponent,
+    ServicesComponent,
+    FilterbarComponent
   ],
   imports: [
     BrowserModule,
