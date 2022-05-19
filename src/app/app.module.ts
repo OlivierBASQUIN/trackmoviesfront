41550a245b265f839e20669e7ed9fb84364c22d8
olivierbasquin <olivier.basquin.iphone@gmail.com>
2022-05-19 12:42:35 +0200

    Creation de la methode getOeuvrById

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
+import { DetailOeuvreComponent } from './detail-oeuvre/detail-oeuvre.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailComponent,
     ListComponent,
     SearchbarComponent,
+    DetailOeuvreComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/detail-oeuvre/detail-oeuvre.component.spec.ts
+++ src/app/detail-oeuvre/detail-oeuvre.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetailComponent } from './detail.component';
+import { DetailOeuvreComponent } from './detail-oeuvre.component';
 
-describe('DetailComponent', () => {
-  let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>;
+describe('DetailOeuvreComponent', () => {
+  let component: DetailOeuvreComponent;
+  let fixture: ComponentFixture<DetailOeuvreComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
+      declarations: [ DetailOeuvreComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailComponent);
+    fixture = TestBed.createComponent(DetailOeuvreComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
