a324e881bc4c4b3ac7d2057813101f674aeb4eb0
fred_epita <frederic.fang@epita.fr>
2022-06-02 11:33:03 +0200

    gestion logout front

--- src/app/shared/services/token-storage.service.ts
+++ src/app/shared/services/token-storage.service.ts
@@ -13,14 +13,19 @@ export class TokenStorageService {
   constructor() { }
 
   signOut(): void {
-    //console.log('signOut');
-    window.sessionStorage.clear();
-      }
+    console.log('signOut');
+    console.log('TOKEN_KEY avant clear/removeItem : ' + window.sessionStorage.getItem(TOKEN_KEY));
+    //sessionStorage.removeItem(TOKEN_KEY);
+    sessionStorage.clear();
+    console.log('TOKEN_KEY après removeItem : ' + window.sessionStorage.getItem(TOKEN_KEY));
+    //sessionStorage.removeItem(USER_KEY);
+    }
 
   public saveToken(token: string): void {
+    console.log('TokenStorageService::saveToken, TOKEN_KEY avant save : ' + window.sessionStorage.getItem(TOKEN_KEY));
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY, token);
-    //console.log('saveToken, TOKEN_KEY : ' + window.sessionStorage.getItem(TOKEN_KEY));
+    console.log('TokenStorageService::saveToken, TOKEN_KEY après save : ' + window.sessionStorage.getItem(TOKEN_KEY));
   }
 
   public getToken(): string | null {
